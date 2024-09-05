9123aae7da62814ac58dffafbf9beff1ed309b8f
Victoria Lim <vtlim@users.noreply.github.com>
2024-09-05 15:17:25 -0700

    protobuf example

--- technical-work/event-stream/confluent-cli.sh
+++ technical-work/event-stream/confluent-cli.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Usage:    ./confluent-cli.sh TOPIC-NAME FORMAT SCHEMA-FILE
 # Example:  ./confluent-cli.sh victoria-avro-registry avro kttm.avro.schema
+# Example:  ./confluent-cli.sh docs-demo-protobuf protobuf kttm.proto
 confluent kafka topic produce $1 --value-format $2 --schema $3
